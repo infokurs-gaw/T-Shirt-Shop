8efa6bd638d50b19b032dfb826dcca4d44ee82e2
Tim Specht <dev@tim-specht.com>
2018-11-17 11:35:15 +0100

    Completed init method so .db files gets generated from sql dump

--- DatabaseAccess.java
+++ DatabaseAccess.java
@@ -2,56 +2,68 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.Path;
+
 /**
  * Beschreiben Sie hier die Klasse DatabaseAccess.
  * 
- * @author (Ihr Name) 
+ * @author (Ihr Name)
  * @version (eine Versionsnummer oder ein Datum)
  */
-public class DatabaseAccess
-{
+public class DatabaseAccess {
     // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
     private DatabaseConnector dbConnector;
 
     /**
      * Konstruktor für Objekte der Klasse DatabaseAccess
      */
-    public DatabaseAccess()
-    {
-        // Instanzvariable initialisi   eren
-        //https://bitbucket.org/xerial/sqlite-jdbc/downloads/ Driver must be installed!!!
+    public DatabaseAccess() {
+        // Instanzvariable initialisi eren
+        // https://bitbucket.org/xerial/sqlite-jdbc/downloads/ Driver must be
+        // installed!!!
         this.init();
     }
 
-    public void init(){
-        try{
-            
+    public void init() {
+        try {
+
             String dbName = "datenbank2.db";
             Path dbPath = Paths.get(dbName);
-            
-            if(!Files.exists(dbPath)){
+
+            if (!Files.exists(dbPath)) {
                 this.dbConnector = new DatabaseConnector("", 0, dbName, "", "");
                 String sql = new String(Files.readAllBytes(Paths.get("create-queries.sql")));
-                this.dbConnector.executeStatement(sql);
-            }else{
+                String[] queries = sql.split(";");
+
+                for (int i = 0; i < queries.length; i++) {
+                    this.dbConnector.executeStatement(queries[i]);
+                }
+
+                System.out.println(
+                        "SQLite Database " + dbName + " should now be successfully initialized and populated. Continuing...");
+
+            } else {
                 System.out.println("Skipping init... db file already exists.");
                 this.dbConnector = new DatabaseConnector("", 0, dbName, "", "");
+
+                String m = this.dbConnector.getErrorMessage();
+
+                System.out.println(m);
             }
-      
-        }catch(Exception e){
+
+        } catch (Exception e) {
             System.out.println(e);
         }
     }
 
-    public static void main(){
+    public static void main() {
         DatabaseAccess d = new DatabaseAccess();
         d.getProducts();
     }
 
-    private String extractField(String fieldName, String[] colNames, String[] row){
+    private String extractField(String fieldName, String[] colNames, String[] row) {
         int index = -1;
-        for(int i = 0; i < row.length; i++){
-            if(colNames[i].equals(fieldName)){
+        for (int i = 0; i < row.length; i++) {
+            if (colNames[i].equals(fieldName)) {
                 index = i;
                 break;
             }
@@ -61,8 +73,9 @@ public class DatabaseAccess
 
     }
 
-    public Product[] getProducts(){
-        this.dbConnector.executeStatement("SELECT s.id, p.name, color, size, type, price, description from stock s JOIN colors c ON s.color_id = c.id JOIN products p on p.id = s.product_id JOIN sizes si ON s.size_id = si.id JOIN product_types pt ON pt.id = p.product_type_id;");
+    public Product[] getProducts() {
+        this.dbConnector.executeStatement(
+                "SELECT s.id, p.name, color, size, type, price, description from stock s JOIN colors c ON s.color_id = c.id JOIN products p on p.id = s.product_id JOIN sizes si ON s.size_id = si.id JOIN product_types pt ON pt.id = p.product_type_id;");
         QueryResult res = this.dbConnector.getCurrentQueryResult();
 
         if (res != null) {
@@ -76,19 +89,16 @@ public class DatabaseAccess
 
                 int id = Integer.parseInt(this.extractField("id", cols, row));
                 double price = Double.parseDouble(this.extractField("price", cols, row));
-                String name = this.extractField("name", cols, row), 
-                color = this.extractField("color", cols, row), 
-                size = this.extractField("size", cols, row), 
-                type = this.extractField("type", cols, row), 
-                description = this.extractField("description", cols, row);
+                String name = this.extractField("name", cols, row), color = this.extractField("color", cols, row),
+                        size = this.extractField("size", cols, row), type = this.extractField("type", cols, row),
+                        description = this.extractField("description", cols, row);
 
                 prods[i] = new Product(id, type, name, description, price, color, size);
 
             }
 
             return prods;
-        } 
-        else {
+        } else {
             System.out.println(dbConnector.getErrorMessage());
 
             return null;
--- create-queries.sql
+++ create-queries.sql
@@ -1,40 +1,94 @@
---
--- File generated with SQLiteStudio v3.1.1 on Mo Nov 12 10:19:02 2018
---
--- Text encoding used: System
---
-PRAGMA foreign_keys = off;
 BEGIN TRANSACTION;
-
--- Table: accounts
-CREATE TABLE accounts (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR (255) NOT NULL, address TEXT NOT NULL, email VARCHAR (255) UNIQUE NOT NULL, password VARCHAR (255) NOT NULL, credit_card VARCHAR (255) UNIQUE);
-
--- Table: colors
-CREATE TABLE colors (id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL, color VARCHAR (255) UNIQUE NOT NULL);
-
--- Table: order_status
-CREATE TABLE order_status (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, status VARCHAR NOT NULL UNIQUE);
-
--- Table: order_stock
-CREATE TABLE order_stock (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, order_id INTEGER REFERENCES orders (id) NOT NULL, stock_id INTEGER NOT NULL REFERENCES stock (id), amount INTEGER NOT NULL DEFAULT (1));
-
--- Table: orders
-CREATE TABLE orders (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, account_id REFERENCES accounts (id), order_date DATETIME DEFAULT (CURRENT_TIMESTAMP) NOT NULL, status_id INTEGER REFERENCES order_status (id));
-
--- Table: product_types
-CREATE TABLE product_types (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, type VARCHAR NOT NULL UNIQUE);
-
--- Table: products
-CREATE TABLE products (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE, name VARCHAR (255) NOT NULL UNIQUE, description TEXT, price DOUBLE NOT NULL, product_type_id INTEGER REFERENCES product_types (id) NOT NULL);
-
--- Table: sizes
-CREATE TABLE sizes (id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL, size VARCHAR (5) UNIQUE NOT NULL);
-
--- Table: stock
-CREATE TABLE stock (id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL, product_id INTEGER NOT NULL REFERENCES products (id), amount INTEGER NOT NULL, color_id INTEGER NOT NULL REFERENCES colors (id), size_id NOT NULL REFERENCES sizes (id));
-
--- View: stock_view
+CREATE TABLE IF NOT EXISTS `stock` (
+	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`product_id`	INTEGER NOT NULL,
+	`amount`	INTEGER NOT NULL,
+	`color_id`	INTEGER NOT NULL,
+	`size_id`	TEXT NOT NULL,
+	FOREIGN KEY(`color_id`) REFERENCES `colors`(`id`),
+	FOREIGN KEY(`size_id`) REFERENCES `sizes`(`id`),
+	FOREIGN KEY(`product_id`) REFERENCES `products`(`id`)
+);
+INSERT INTO `stock` VALUES (1,1,10,1,1),
+ (2,1,5,1,4);
+CREATE TABLE IF NOT EXISTS `sizes` (
+	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`size`	VARCHAR ( 5 ) NOT NULL UNIQUE
+);
+INSERT INTO `sizes` VALUES (1,'XS'),
+ (2,'S'),
+ (3,'M'),
+ (4,'L'),
+ (5,'XL'),
+ (6,'XXL');
+CREATE TABLE IF NOT EXISTS `products` (
+	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`name`	VARCHAR ( 255 ) NOT NULL UNIQUE,
+	`description`	TEXT,
+	`price`	DOUBLE NOT NULL,
+	`product_type_id`	INTEGER NOT NULL,
+	FOREIGN KEY(`product_type_id`) REFERENCES `product_types`(`id`)
+);
+INSERT INTO `products` VALUES (1,'Infokurs T-Shirt',NULL,19.95,1);
+CREATE TABLE IF NOT EXISTS `product_types` (
+	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	`type`	VARCHAR NOT NULL UNIQUE
+);
+INSERT INTO `product_types` VALUES (1,'T-Shirt'),
+ (2,'Pullover'),
+ (3,'Hose');
+CREATE TABLE IF NOT EXISTS `orders` (
+	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	`account_id`	TEXT,
+	`order_date`	DATETIME NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+	`status_id`	INTEGER,
+	FOREIGN KEY(`status_id`) REFERENCES `order_status`(`id`),
+	FOREIGN KEY(`account_id`) REFERENCES `accounts`(`id`)
+);
+INSERT INTO `orders` VALUES (1,1,'2018-10-29 09:04:58',1);
+CREATE TABLE IF NOT EXISTS `order_stock` (
+	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	`order_id`	INTEGER NOT NULL,
+	`stock_id`	INTEGER NOT NULL,
+	`amount`	INTEGER NOT NULL DEFAULT (1),
+	FOREIGN KEY(`stock_id`) REFERENCES `stock`(`id`),
+	FOREIGN KEY(`order_id`) REFERENCES `orders`(`id`)
+);
+INSERT INTO `order_stock` VALUES (1,1,1,1),
+ (2,1,2,2);
+CREATE TABLE IF NOT EXISTS `order_status` (
+	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	`status`	VARCHAR NOT NULL UNIQUE
+);
+INSERT INTO `order_status` VALUES (1,'AUFGEBENEN'),
+ (2,'BEARBEITET'),
+ (3,'VERSANDT'),
+ (4,'ERHALTEN'),
+ (5,'ABGESCHLOSSEN');
+CREATE TABLE IF NOT EXISTS `event_log` (
+	`id`	INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`account_id`	INTEGER,
+	`event`	VARCHAR ( 255 ) NOT NULL,
+	`timestamp`	DATETIME DEFAULT (CURRENT_TIMESTAMP),
+	FOREIGN KEY(`account_id`) REFERENCES `accounts`(`id`)
+);
+INSERT INTO `event_log` VALUES (1,1,'LOGIN','2018-11-17 09:51:01'),
+ (2,1,'LOGIN','2018-11-17 09:51:55'),
+ (3,1,'LOGOUT','2018-11-17 09:52:41');
+CREATE TABLE IF NOT EXISTS `colors` (
+	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`color`	VARCHAR ( 255 ) NOT NULL UNIQUE
+);
+INSERT INTO `colors` VALUES (1,'schwarz'),
+ (2,'rot');
+CREATE TABLE IF NOT EXISTS `accounts` (
+	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	`name`	VARCHAR ( 255 ) NOT NULL,
+	`address`	TEXT NOT NULL,
+	`email`	VARCHAR ( 255 ) NOT NULL UNIQUE,
+	`password`	VARCHAR ( 255 ) NOT NULL,
+	`credit_card`	VARCHAR ( 255 ) UNIQUE
+);
+INSERT INTO `accounts` VALUES (1,'Celina','Bla Bla','celina@gaw.de','bieeeeebg','2554738');
 CREATE VIEW stock_view AS SELECT s.id, p.name, color, size, amount from stock s JOIN colors c ON s.color_id = c.id JOIN products p on p.id = s.product_id JOIN sizes si ON s.size_id = si.id;
-
-COMMIT TRANSACTION;
-PRAGMA foreign_keys = on;
+COMMIT;
