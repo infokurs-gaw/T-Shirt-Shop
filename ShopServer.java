578b66532228fc9022668d679fdb220907dbe811
Tim Specht <infokurs-gaw@gmx.de>
2018-11-26 10:22:43 +0100

    Completed first methods

--- ShopServer.java
+++ ShopServer.java
@@ -3,7 +3,7 @@ import java.util.*;
 import java.io.*;
 
 public class ShopServer extends Server {
-
+    DatabaseAccess db = new DatabaseAccess();
     Map<String, Account> accounts = new HashMap<String, Account>();
 
     String clientIP;
@@ -15,7 +15,7 @@ public class ShopServer extends Server {
     public void processNewConnection(String pClientIP, int pClientPort) {
         clientIP = pClientIP;
 
-        send(pClientIP, pClientPort, "Willkommen! W�hlen Sie eine Gr��e und eine Farbe f�r Ihr T-Shirt.");
+        send(pClientIP, pClientPort, "Willkommen in unserem T-Shirt Shop!!!");
     }
 
     public void processMessage(String pClientIP, int pClientPort, String pMessage) {
@@ -24,22 +24,22 @@ public class ShopServer extends Server {
         String[] nachrichtTeil = pMessage.split(":");
 
         if(nachrichtTeil[0].equals("LOGIN")){
-            logIn(pClientIP,nachrichtTeil[1],nachrichtTeil[2]);
+            logIn(pClientIP,pClientPort,nachrichtTeil[1],nachrichtTeil[2]);
         }
 
         else if(nachrichtTeil[0].equals("REGISTER")) {
-            register(Integer.valueOf(nachrichtTeil[1]),nachrichtTeil[2],nachrichtTeil[3],nachrichtTeil[4],nachrichtTeil[5],nachrichtTeil[6]);
+            register(pClientIP, pClientPort, Integer.valueOf(nachrichtTeil[1]),nachrichtTeil[2],nachrichtTeil[3],nachrichtTeil[4],nachrichtTeil[5],nachrichtTeil[6]);
         }
 
         else if(nachrichtTeil[0].equals("ADD")) {
 
-            addNewProducts(pClientIP, Integer.valueOf(nachrichtTeil[1]), Integer.valueOf(nachrichtTeil[2]));
+            addNewProducts(pClientIP, pClientPort, Integer.valueOf(nachrichtTeil[1]), Integer.valueOf(nachrichtTeil[2]));
 
         }
 
         else if(nachrichtTeil[0].equals("BUY")) {
 
-            buy(pClientIP);
+            buy(pClientIP, pClientPort);
 
         }
 
@@ -52,12 +52,12 @@ public class ShopServer extends Server {
         //         if(nachrichtTeil[0].equals("TSHIRT")) {
         //             send(pClientIP, pClientPort, "Die Groesse ist " + nachrichtTeil[1] + 
         //                                          ", die Farbe ist " + nachrichtTeil[2] + 
-        //                                          " und es kostet 19,99 Euro! Bitte best�tigen Sie die Bestellung.");
+        //                                          " und es kostet 19,99 Euro! Bitte bestätigen Sie die Bestellung.");
         //         }
         // 
         //         else if(nachrichtTeil[0].equals("BESTAETIGUNG")) {
         //             if(nachrichtTeil [1] .equals("ja")) {  
-        //                 send (pClientIP, pClientPort, "Vielen Dank f�r Ihre Bestellung."); 
+        //                 send (pClientIP, pClientPort, "Vielen Dank für Ihre Bestellung."); 
         //                 closeConnection(pClientIP, pClientPort);  
         //             }
         //             else if (nachrichtTeil[1].equals("nein")) {
@@ -81,55 +81,31 @@ public class ShopServer extends Server {
         this.send(pClientIP, pClientPort, "Auf Wiedersehen!");
     }
 
-    public void register(int id, String name, String address, String email, String creditCard, String pass){
+    public void register(String pClientIP, int pClientPort, int id, String name, String address, String email, String creditCard, String pass){
         Account newAccount = new Account(id, name, address, email, creditCard, null);
 
-        /*
-         *  Hier Account zu Datenbank hinzuf?gen!
-         *  
-         *  
-         */
+        db.addAccount(newAccount,pass);
 
         accounts.put(clientIP, newAccount);
     }
 
-    public void logIn(String pClientIP, String email, String pass){
-        /*
-         * Account newAccount = db.getAccount(email);
-         * 
-         * accounts.put(clientIP, newAccount);
-         * 
-         * ba.startTimer(accounts.get(pClientIP));
-         */
+    public void logIn(String pClientIP, int pClientPort, String email, String pass){
+        if(db.getAccount(email,pass)!=null){
+            Account newAccount = db.getAccount(email,pass);
+
+            accounts.put(clientIP, newAccount);
+
+        }
+        else{
+            send(pClientIP, pClientPort, "Nutzername oder Passwort falsch! Bitte überprüfen Sie Ihre Eingabe");
+        }
+
+        //ba.startTimer(accounts.get(pClientIP));
     }
 
-    public void addNewProducts(String pClientIP, int id, int amount) {
-        /*
-         * 
-         * 
-         * Product newProduct = db.getProduct(id);
-         * 
-         * basket.addProduct(newProduct, amount, accounts.get(pClientIP) );
-         * 
-         */
-    }
+    public void addNewProducts(String pClientIP, int pClientPort, int id, int amount) {
+        int realAmount=-1;
+        if(db.getProductById(id)!=null){
+            Product newProduct = db.getProductById(id);
 
-    public void buy(String pClientIP){
-        /* 
-         * Order newOrder = new Order(-1, basket.getProducts(), accounts.get(pClientIP), "AUFGEGEBEN");
-         * 
-         * db.addOrder(newOrder);
-         * 
-         */
-    }
-
-    public void logOff(String pClientIP, int pClientPort){
-        accounts.remove(pClientIP);
-        send(pClientIP, pClientPort, "gggTsch�ss!!!");
-        //closeConnection(pClientIP, pClientPort);
-        /*
-         * ba.stopTimer(accounts.get(pClientIP));
-         * 
-         */
-    }
-}
+            i
