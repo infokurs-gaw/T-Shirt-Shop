64ec28a9b798d621f83c7886e5a76658bb661fbd
Tim Specht <mail@tim-specht.com>
2018-12-01 11:07:49 +0100

    Added timeout

--- ShopClient.java
+++ ShopClient.java
@@ -1,8 +1,9 @@
 import java.util.HashMap;
+import java.util.Date;
 
 public class ShopClient extends Client {
 
-    private String lastMessage;
+    private String lastMessage = "";
 
     public ShopClient(String pServerIP, int pServerPort) {
         super(pServerIP, pServerPort);
@@ -43,13 +44,23 @@ public class ShopClient extends Client {
         ProtocolDefinition def = new ProtocolDefinition("PRODUCT_INFO:<id>:<type>:<name>:<desc>:<price>:<color>:<size>",
                 null);
 
-        while (!def.matchesDefinition(lastMessage)) {
+        Date startDate = new Date();
+
+        while (!def.matchesDefinition(lastMessage) && (new Date().getTime()-startDate.getTime())/1000 < 10) {
             //System.out.println("WAITING...");
         }
 
-        HashMap<String, String> map = def.extractInformation(lastMessage);
 
-        return new Product(Integer.parseInt(map.get("id")), map.get("type"), map.get("name"), map.get("desc"),
-                Double.parseDouble(map.get("price")), map.get("color"), map.get("size"));
+        if((new Date().getTime()-startDate.getTime())/1000 >= 10){
+            System.out.println("Could not get Product, timeout of 10 seconds expired.");
+            return null;
+        }else{
+            HashMap<String, String> map = def.extractInformation(lastMessage);
+
+            return new Product(Integer.parseInt(map.get("id")), map.get("type"), map.get("name"), map.get("desc"),
+                    Double.parseDouble(map.get("price")), map.get("color"), map.get("size"));
+        }
+
+       
     }
 }
